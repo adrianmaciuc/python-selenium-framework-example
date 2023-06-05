50a924113f5b857900f508324145ef8e12dce2f3
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-06-05 23:11:58 +0300

    add to base get wait for loading spinne r

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,19 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 class BasePage:
+    LOADING_SPINNER = (By.CSS_SELECTOR , '[title="Loading..."]')
 
     def __init__(self, browser):
         self.browser = browser
 
-    def get(self, locator):
-        return WebDriverWait(self.browser, 15).until(EC.element_to_be_clickable(locator))
+    def get(self, *locator):
+        return WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(locator))
 
-    def click(self, by_locator):
-        self.get(by_locator).click()
+    def click(self, *by_locator):
+        self.get(*by_locator).click()
+
+    def wait_for_loading_spinner(self):
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.LOADING_SPINNER))
+        WebDriverWait(self.browser, 10).until_not(EC.presence_of_element_located(self.LOADING_SPINNER))
