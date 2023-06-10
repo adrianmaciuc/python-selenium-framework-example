27d85d7d2744a06cd0b6f5e436294e4626de70d3
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-06-11 02:39:50 +0300

    add get_multiple

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,12 +12,15 @@ class BasePage:
     def get(self, *locator):
         return WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located(locator))
 
+    def get_multiple(self, *locator):
+        return self.browser.find_elements(*locator)
+
     def click(self, *by_locator):
         self.get(*by_locator).click()
+
+    def contains(self, text):
+        return self.browser.find_element(By.XPATH, f'//*[contains(text(),"{text}")]')
 
     def wait_for_loading_spinner(self):
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.LOADING_SPINNER))
         WebDriverWait(self.browser, 10).until_not(EC.presence_of_element_located(self.LOADING_SPINNER))
-
-    def contains(self, text):
-        return self.browser.find_element(By.XPATH, f'//*[contains(text(),"{text}")]')
