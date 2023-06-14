619fae082f39c7a355ca6106fe861e71f2fca8a5
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-06-15 01:04:14 +0300

    wrap up test for sorting and filters

--- tests/test_filter_products.py
+++ tests/test_filter_products.py
@@ -1,4 +1,6 @@
 import pytest
+import time
+import re
 from sys import platform
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -27,14 +29,26 @@ def test_filter_products(browser):
 
     assert product_catalog.get_number_of_products() == 12
 
-    product_catalog.select_filter_option_by_text("Style")
-    # product_catalog.select_filter_subcategory_option_by_text("Hoodie")
-    product_catalog.wait_for_page_to_load()
-
+    product_catalog.filter_shopping_option_by("Style")
+    product_catalog.filter_subcategory_of_shopping_option_by("Hoodie")
+    time.sleep(1)
     assert product_catalog.get_number_of_products() == 9
 
-    product_catalog.select_filter_option_by_text("Price")
-    # product_catalog.select_filter_subcategory_option_by_text("$60.00 and above")
-    product_catalog.wait_for_page_to_load()
-
+    product_catalog.filter_shopping_option_by("Price")
+    product_catalog.filter_subcategory_of_shopping_option_by("$60.00")
+    time.sleep(1)
     assert product_catalog.get_number_of_products() == 1
+
+
+def test_sort_products_by_price(browser):
+    product_catalog = ProductCatalog(browser)
+    product_catalog.load('women/tops-women/tanks-women.html')
+    value_before_sort = product_catalog.get_price_of_product_nth(1)
+    time.sleep(1)
+
+    product_catalog.sort_by('Price')
+    value_after_sort = product_catalog.get_price_of_product_nth(1)
+
+    time.sleep(1)
+
+    assert int(re.sub(r'\..*|\$', '', value_before_sort)) > int(re.sub(r'\..*|\$', '', value_after_sort))
